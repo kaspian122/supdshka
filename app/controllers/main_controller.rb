432b50ffe1e399a4ea159c71b245e007bc367d45
kaspian122 <kaspian122@gmail.com>
2018-08-21 17:58:14 +0700

    Supd on rails v 0.34

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,42 +3,42 @@ class MainController < ApplicationController
   layout 'main_templayte'
 
   def index
-    @title = "supd"
+    @title = "О системе"
     @pagenumb = 1
 
   end
 
   def about_company
-    @title = "supd"
+    @title = "О компании"
     @pagenumb = 2
   end
 
   def uslugi
-    @title = "supd"
+    @title = "Услуги"
     @pagenumb = 4
   end
 
   def vitrual_tour
-    @title = "supd"
+    @title = "Виртуальный тур"
     @pagenumb = 3
   end
 
   def buy
     @comment = Comment.new
-    @title = "supd"
+    @title = "Заказать систему"
     @pagenumb = 5
 
   end
 
   def create
-      @title = "supd"
+      @title = "Заказать систему"
       @pagenumb = 5
       @comment = Comment.new(comment_params)
       if @comment.valid?
         if params[:commit] == 'Отправить'
           @comment.save
           QwerryMailer.with(data: @comment).quwery_email.deliver_now
-          @title = "supd"
+          @title = "Контакты"
           @pagenumb = 7
           redirect_to contacts_path and return
         end
@@ -47,7 +47,7 @@ class MainController < ApplicationController
         redirect_to buy_path
       else
         if params[:commit] == 'Отправить'
-          @title = "supd"
+          @title = "Контакты"
           @pagenumb = 7
           render 'contacts' and return
         end
@@ -57,13 +57,13 @@ class MainController < ApplicationController
   end
 
   def faq
-    @title = "supd"
+    @title = "FAQ"
     @pagenumb = 6
   end
 
   def contacts
     @comment = Comment.new
-    @title = "supd"
+    @title = "Контакты"
     @pagenumb = 7
   end
 
